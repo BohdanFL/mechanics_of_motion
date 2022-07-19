97297806b3614e92ffbee45e0636de7a32ba1c7d
BohdanFlexer <robot228rb@gmail.com>
2022-07-19 15:27:46 +0300

    fix

--- js/store.js
+++ js/store.js
@@ -4,37 +4,37 @@ const shopUnits = [
 		brand: null,
 		priceBuy: 10000,
 		priceSell: 10000*0.75
-	}
+	},
 	{
 		name: 'Tractor 2',
 		brand: null,
 		priceBuy: 20000,
 		priceSell: 20000*0.75
-	}
+	},
 	{
 		name: 'Tractor 3 ',
 		brand: null,
 		priceBuy: 30000,
 		priceSell: 30000*0.75
-	}
+	},
 	{
 		name: 'Harverster 1 ',
 		brand: null,
 		priceBuy: 35000,
 		priceSell: 35000*0.75
-	}
+	},
 	{
 		name: 'Car 1',
 		brand: null,
 		priceBuy: 30000,
 		priceSell: 30000*0.75
-	}
+	},
 	{
 		name: 'Tractor 3 ',
 		brand: null,
 		priceBuy: 30000,
 		priceSell: 30000*0.75
-	}
+	},
 	{
 		name: 'Tractor 3 ',
 		brand: null,
